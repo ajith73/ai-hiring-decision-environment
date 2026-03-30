c2e9ce5cbb14329b5c199fe156efabc257242ee6
Ajith <ajith73@users.noreply.huggingface.co>
2026-03-30 06:57:04 +0000

    Upload folder using huggingface_hub

--- server/__init__.py
+++ server/__init__.py
@@ -1,11 +1,2 @@
-# Copyright (c) Meta Platforms, Inc. and affiliates.
-# All rights reserved.
-#
-# This source code is licensed under the BSD-style license found in the
-# LICENSE file in the root directory of this source tree.
-
-"""Hiring Env environment server components."""
-
-from .hiring_env_environment import HiringEnvironment
-
-__all__ = ["HiringEnvironment"]
+# Hiring Env environment server components.
+# This file is intentionally simple to avoid circular imports and loading of legacy components.
